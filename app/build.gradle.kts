995daf71bc0be9e9918da1cf13b4a171932ad0af
Piyush Suthar <piyushsuthar100@gmail.com>
2026-07-10 22:58:56 +0530

    Fix github action gradlew permission and disable minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
